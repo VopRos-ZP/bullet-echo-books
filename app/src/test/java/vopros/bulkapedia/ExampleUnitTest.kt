387248cc7b1c5b93da567b9c5bdee4e8e011afa7
VopRos-ZP <zolpavel366@gmail.com>
2023-10-22 13:43:03 +0300

    added test for calculate `Effect`

--- app/src/test/java/vopros/bulkapedia/ExampleUnitTest.kt
+++ app/src/test/java/vopros/bulkapedia/ExampleUnitTest.kt
@@ -12,6 +12,17 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val numbers = mapOf(
+            "0" to intArrayOf(0, 3, 3, 6, 6, 9, 9, 12, 12, 13, 13),
+            "1" to intArrayOf(0, 0, 1, 1, 3, 3, 4, 4, 6, 6, 7),
+        )
+        val startNumbers = mapOf(
+            "0" to 53,
+            "1" to 6
+        )
+        val result = numbers.mapValues { (k, arr) ->
+            arr.map { startNumbers[k]!! + it }.toIntArray()
+        }
+        assertArrayEquals(result["0"], intArrayOf(53, 56, 56, 59, 59, 62, 62, 65, 65, 66, 66))
     }
 }
